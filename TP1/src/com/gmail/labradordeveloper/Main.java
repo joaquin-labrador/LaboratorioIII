45567f79831333eeb993502b850a56f52a911761
joaquin <labradordeveloper@gmail.com>
2022-03-17 18:09:37 -0300

    Exercise 15

--- TP1/src/com/gmail/labradordeveloper/Main.java
+++ TP1/src/com/gmail/labradordeveloper/Main.java
@@ -2,30 +2,57 @@
     Seleccionar palabra + Alt + J –> Selecciona la siguiente coincidencia y se editan en conjunto
  */
 
-
 package com.gmail.labradordeveloper;
+
 import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        int N ;
+        double R;
+        double c1;
+        double c2;
         /*exerciseOne();
         exerciseTwo();
         exerciseThree();*/
         //exerciseFour();
-        Scanner sc = new Scanner((System.in));
-        int N ;
-        do {
-            System.out.println("Enter a number different than 0");
-            N = sc.nextInt();
-        } while (N == 0);
-        exercise7(N);
-        System.out.println("Introduce your name");
-        sc.nextLine();//Me muevo en la consola
-        //sc.next() me guarda hasta encontrar un espacio
-        String name = sc.nextLine();
-        showName(name);
-        System.out.println("Enter a number");
-        N = sc.nextInt();
-        tripleDouble(N);
+//       do {
+//            System.out.println("Enter a number different than 0");
+//            N = sc.nextInt();
+//        } while (N == 0);
+//        exercise7(N);
+//        System.out.println("Introduce your name");
+//        sc.nextLine();//Me muevo en la consola
+//        //sc.next() me guarda hasta encontrar un espacio
+//        String name = sc.nextLine();
+//        showName(name);
+        //tripleDouble(N);
+        //System.out.println((farToCelsius(N)));
+//        System.out.println("Enter the radius");
+//        radioToArea(R);
+//        System.out.println("Enter a velocity in Km/H");
+//        R = sc.nextDouble();
+//        System.out.println();
+//        System.out.println((kmHToKmS(R) != 0  ?   Math.round(kmHToKmS(R) * 100.0)/100.0  + " Km/s" : " Error"));
+//        System.out.println("Enter a cathetus 1");
+//        c1 = sc.nextDouble();
+//        System.out.println("Enter a cathetus 2");
+//        c2 = sc.nextDouble();
+//        System.out.println(hypotenuse(c1,c2) != 0 ? "Hypotenuse " + Math.round(hypotenuse(c1,c2) * 100.0)/100.0 + " cm" : "Error");
+
+        System.out.println("Enter the radius");
+        R = sc.nextDouble();
+        System.out.println(volumeSphere(R) != 0 ? "Volume : " + Math.round(volumeSphere(R) * 100.0) / 100.0 : "Error");
+        double S1 , S2 , S3;
+        System.out.println("Enter the sides of a triangle");
+        S1 = sc.nextDouble();
+        S2 = sc.nextDouble();
+        S3 = sc.nextDouble();
+        System.out.println(areaTriangle(S1,S2,S3) != 0? "Area : " + (areaTriangle(S1,S2,S3)): "Error");
+
+
 
     }
 
@@ -99,27 +126,61 @@ public class Main {
     public static void exercise7(int N) {
         System.out.println(N + " is " + (evenOrOdd(N) ? "Even" : "Odd"));
         System.out.println(N + " is " + (negativeOrPositive(N) ? "Positive" : "Negative"));
-        System.out.println(N + " is " + (multipleOf(N,5) ? "multiple of 5" : "not multiple of 5" ));
-        System.out.println(N + " is " + (multipleOf (N ,10) ? "multiple of 10" : "not multiple of 10"));
-        System.out.println(N + " is " + (lessThan(N , 100) ? "greater or equal to 100" : "less than 100"));
+        System.out.println(N + " is " + (multipleOf(N, 5) ? "multiple of 5" : "not multiple of 5"));
+        System.out.println(N + " is " + (multipleOf(N, 10) ? "multiple of 10" : "not multiple of 10"));
+        System.out.println(N + " is " + (lessThan(N, 100) ? "greater or equal to 100" : "less than 100"));
     }
 
-    public static boolean multipleOf (int N, int M){
+    public static boolean multipleOf(int N, int M) {
         return N % M == 0;
     }
-    public static boolean lessThan(int N, int higher){
+
+    public static boolean lessThan(int N, int higher) {
         return N >= higher;
     }
+
     //Exercise 8
-    public static void showName(String name){
+    public static void showName(String name) {
         System.out.println("Hi! " + name);
     }
+
     //Exercise 9
-    public static void tripleDouble (int N){
-        System.out.println( "The double of " + N + " is " + N*2);
-        System.out.println( "The triple of " + N + " is " + N*3);
+    public static void tripleDouble(int N) {
+        System.out.println("The double of " + N + " is " + N * 2);
+        System.out.println("The triple of " + N + " is " + N * 3);
     }
 
+    //Exercise 10
+    public static double farToCelsius(int fahrenheit) {
+        return ((double) (fahrenheit - 32) * 5 / 9);
+
+    }
+    //Exercise 11
+    public static void radioToArea(double R){
+        System.out.println("Area " + Math.PI * Math.pow(R , 2) + " cm");
+        System.out.println("Length " + Math.PI * R * 2 + " cm");
+    }
+    //Exercise 12
+    public static double kmHToKmS (double velocity){
+
+        return (velocity > 0) ? velocity / 3600 : 0;
+    }
+    //Exercise 13
+    public static double hypotenuse (double c1 , double c2){
+        return (c1 > 0 && c2 > 0) ? Math.sqrt(Math.pow(c1 , 2) + Math.pow(c2 , 2)): 0;
+    }
+    //Exercise 14
+    public static double volumeSphere(double R){
+        return (R > 0) ? ( 4 * Math.PI  * Math.pow(R , 3) / 3) : 0;
+    }
+    //Exercise 15
+    public static double areaTriangle(double A1,double A2,double A3){
+        if(A1 > 0 && A2 > 0 && A3 > 0 ){
+            double sP = (A1 + A2 + A3) / 2;
+            return Math.round(Math.sqrt(sP * (sP - A1) * (sP - A2) * (sP - A3))* 100.0) / 100.0;
+        }
+        return 0;
+    }
 }
 
 
