b3257501762c68f7a9245139d1c1362dc92d244c
joaquin <labradordeveloper@gmail.com>
2022-03-25 10:43:23 -0300

    Start with Tp3

--- Tp2/src/Main.java
+++ Tp2/src/Main.java
@@ -6,6 +6,7 @@ import Exercise4.BankAccount;
 import Exercise5.Hour;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("\n==== Exercise 1 ==== \n");
         Rectangle R = new Rectangle(20, 10);
@@ -23,6 +24,7 @@ public class Main {
 
         System.out.println("\n==== Exercise 3 ==== \n");
         Item goudaChesse = new Item(12654, "Foodstuffs", 180, 3.25);
+       // System.out.println(goudaChesse.hashCode()); direccion de memoria del objeto
         System.out.println(goudaChesse);
 
         System.out.println("\n==== Exercise 4 ==== \n");
@@ -36,11 +38,10 @@ public class Main {
 
         System.out.println("\n==== Exercise 5 ==== \n");
         Hour W = new Hour(13, 4, 22);
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 40; i++) {
             W.downSecond();
             System.out.println(W);
         }
-
 
     }
 
